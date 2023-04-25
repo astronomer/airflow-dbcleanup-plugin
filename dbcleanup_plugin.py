53ce43c2ddbb3dadb572c8dadd96a4b8b6d6d986
pgvishnuram <vishnu@astronomer.io>
2023-04-26 02:14:33 +0530

    add custom permission for dbcleanup plugin

--- dbcleanup_plugin.py
+++ dbcleanup_plugin.py
@@ -18,12 +18,11 @@ from airflow.utils.session import NEW_SESSION, provide_session
 from airflow.utils import db_cleanup, dates
 from airflow.utils.db_cleanup import config_dict
 from airflow.settings import conf
-from airflow.security import permissions
 from airflow.www import auth
 
 from .cloud_providers import ProviderFactory
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 log = logging.getLogger(__name__)
 
@@ -323,24 +322,24 @@ def export_cleaned_records(
 
 # Creating a flask appbuilder BaseView
 class AstronomerDbcleanup(AppBuilderBaseView):
+    resource_name = "dbcleanup"
     default_view = "dbcleanup"
+    csrf_exempt = False
+    base_permissions = ["can_access_dbcleanup"]
+    # A bug in Flask-AppBuilder mandates us to have a value for method_permission_name
+    # before is_item_public filter can be used.
+    method_permission_name = {
+        "access_dbcleanup": "access_dbcleanup",
+    }
+    allow_browser_login = True
 
     @expose("api/v1/dbcleanup", methods=["POST", "GET"])
     @auth.has_access(
         [
-            (permissions.ACTION_CAN_READ, permissions.RESOURCE_TASK_RESCHEDULE),
-            (permissions.ACTION_CAN_ACCESS_MENU, permissions.RESOURCE_TASK_RESCHEDULE),
-            (permissions.ACTION_CAN_READ, permissions.RESOURCE_TRIGGER),
-            (permissions.ACTION_CAN_ACCESS_MENU, permissions.RESOURCE_TRIGGER),
-            (permissions.ACTION_CAN_READ, permissions.RESOURCE_PASSWORD),
-            (permissions.ACTION_CAN_EDIT, permissions.RESOURCE_PASSWORD),
-            (permissions.ACTION_CAN_READ, permissions.RESOURCE_ROLE),
-            (permissions.ACTION_CAN_EDIT, permissions.RESOURCE_ROLE),
+            ("can_access_dbcleanup", "AstronomerDbcleanup"),
         ]
     )
     @csrf.exempt
-    # disabled jwt auth for rest point
-    # @jwt_token_secure
     def tasks(self):
         try:
             success, release, provider, e, msg = _airflow_dbexport()
